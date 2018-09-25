a8c6c4171460b147ba03d45422edf579193faa1c
Rigamorphius <43024627+Rigamorphius@users.noreply.github.com>
2018-09-24 21:51:10 -0500

    Completed PlayerMovement Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(MoveHorizontal,0.0f,MoveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
